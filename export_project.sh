6e0bffc746a7165489b257e4d9508139a0b80857
Clément Hamada <chamada@student.le-101.fr>
2020-09-23 00:26:08 +0200

    Add export_projects

---
+++ export_project.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+EXPORT_PRESET="HTML5"
+NAME="Game"
+BRANCH="master"
+PROJECT="${HOME}/build/${NAME}/${BRANCH}"
+VERSION="$(git describe || echo 0.0)"
+DESTINATION="/srv/http/${NAME}/${BRANCH}/${VERSION}/index.html"
+
+cd "${PROJECT}"
+godot --export "${EXPORT_PRESET}" "${DESTINATION}"
+
--- install_project.sh
+++ install_project.sh
@@ -6,9 +6,7 @@ set -e
 GIT_URL="https://github.com/Ludrak/ready_player_42.git"
 GIT_BRANCH="master"
 NAME="Game"
-VERSION="$(git describe || echo 0.0)"
-#DESTINATION="/srv/http/${NAME}/${BRANCH}/${VERSION}/index.html"
-DESTINATION="/${HOME}/build/${NAME}/${BRANCH}/${VERSION}"
+DESTINATION="/${HOME}/build/${NAME}/${BRANCH}/"
 
 echo "Updating yum..."
 sudo yum update -y
@@ -18,5 +16,9 @@ sudo yum install git -y
 
 mkdir -p "${DESTINATION}"
 
-git clone "${GIT_URL}" --branch "${GIT_BRANCH}" "${DESTINATION}"
-
+if [ -d ${DESTINATION} ]; then
+	cd "${DESTINATION}"
+	git pull
+else
+	git clone "${GIT_URL}" --branch "${GIT_BRANCH}" "${DESTINATION}"
+fi
