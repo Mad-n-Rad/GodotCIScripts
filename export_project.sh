3d18aa1e26ae3ac49972818295ca0f96b8f59925
Clément Hamada <chamada@student.le-101.fr>
2020-09-23 01:12:00 +0200

    export_project: Redirect failed describe to /dev/null

--- export_project.sh
+++ export_project.sh
@@ -7,7 +7,7 @@ EXPORT_PRESET="HTML5"
 NAME="Game"
 BRANCH="master"
 PROJECT="${HOME}/build/${NAME}/${BRANCH}"
-VERSION="$(git describe || echo 0.0)"
+VERSION="$(git describe 2>/dev/null || echo 0.0)"
 DESTINATION="/srv/http/${NAME}/${BRANCH}/${VERSION}/index.html"
 
 cd "${PROJECT}"
