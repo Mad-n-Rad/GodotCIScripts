685368c76c9d447e5027827004b649bcbdef4010
Clément Hamada <chamada@student.le-101.fr>
2020-09-23 01:36:09 +0200

    export_project: Create destination directory

--- export_project.sh
+++ export_project.sh
@@ -11,5 +11,6 @@ VERSION="$(git describe 2>/dev/null || echo 0.0)"
 DESTINATION="/srv/http/${NAME}/${BRANCH}/${VERSION}/index.html"
 
 cd "${PROJECT}"
+mkdir -p $(dirname "${DESTINATION}")
 godot --export "${EXPORT_PRESET}" "${DESTINATION}"
 
